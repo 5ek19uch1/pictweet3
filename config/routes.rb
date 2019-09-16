3bb8910cb536fcc2bc2018512a8f84aacc57d600
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-09-16 09:44:36 +0900

    詳細画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'tweets#index'                       #ルートパスの指定
+
   get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
   get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  delete'tweets/:id'  => 'tweets#destroy'    #ツイート削除機能
+  patch   'tweets/:id'  => 'tweets#update'   #ツイートの更新
+  get   'tweets/:id/edit'  => 'tweets#edit'  #ツイート編集機能
+  get   'users/:id'   =>  'users#show'       #Mypageへのルーティング
 end
